10e0b8828cf975a4a7f55de01c44289fabc402b3
Lypher <acosta.enzo@hotmail.com>
2023-03-19 21:07:19 -0300

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import styles from "../components/About.module.css";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className={styles.texto}>
+      ¡Hola! Soy Enzo Acosta, un estudiante de Henry que está interesado en
+      aprender todo lo posible sobre desarrollo web. Actualmente estoy
+      asistiendo a un bootcamp en el que estoy aprendiendo sobre tecnologías
+      como React, Redux, JavaScript para front-end y back-end, y estoy
+      emocionado de poner en práctica todo lo que estoy aprendiendo. Esta página
+      web es el resultado de mi trabajo en el bootcamp, y la estoy usando para
+      practicar lo que he aprendido en clase. Mi objetivo es crear un sitio web
+      que sea tanto funcional como estéticamente agradable, y en el proceso,
+      continuar mejorando mis habilidades de programación. En mi tiempo libre,
+      me gusta seguir aprendiendo sobre tecnología y participar en proyectos
+      relacionados con la programación. Me encanta enfrentarme a desafíos y
+      resolver problemas complejos de manera creativa. Creo que la programación
+      es una herramienta poderosa para crear soluciones útiles y únicas, y me
+      encanta ser parte de esa comunidad. Si estás interesado en colaborar en
+      proyectos o simplemente quieres charlar sobre tecnología, ¡no dudes en
+      ponerte en contacto conmigo!
+    </div>
+  );
 }
