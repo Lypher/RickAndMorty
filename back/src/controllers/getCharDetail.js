6e72b5e3c2537df8f1bc284baca78ae4eb885994
Lypher <acosta.enzo@hotmail.com>
2023-03-24 12:35:56 -0300

    todo ok

--- back/src/controllers/getCharDetail.js
+++ back/src/controllers/getCharDetail.js
@@ -1,11 +1,14 @@
+const axios = require("axios");
+
 const getCharDetail = (res, id) => {
   axios(`https://rickandmortyapi.com/api/character/${id}`)
     .then((result) => result.data)
     .then((data) => {
       let character = {
+        id: data.id,
         name: data.name,
         image: data.image,
-        gender: data.image,
+        gender: data.gender,
         species: data.species,
         status: data.status,
         origin: data.origin.name,
@@ -14,8 +17,10 @@ const getCharDetail = (res, id) => {
         .writeHead(200, { "Content-Type": "application/json" })
         .end(JSON.stringify(character));
     })
-    .catch((err) => res.writeHead(500, { "Content-Type": "text/plain" }))
-    .end("Error id no encontrado");
+    .catch((err) => {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error id no encontrado");
+    });
 };
 
 module.exports = getCharDetail;
